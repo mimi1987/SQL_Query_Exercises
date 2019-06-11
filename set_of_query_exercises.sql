795a9de787cc6a3ce3f25154fce3ea1d7e3ca893
Michael M <m.migsch@gmail.com>
2019-06-11 19:14:18 +0200

    Update set_of_query_exercises.sql

--- set_of_query_exercises.sql
+++ set_of_query_exercises.sql
@@ -1,3 +1,7 @@
 -- Chapter 2 not all exercises included, because I started this repository a bit later.
 
+-- Exercise 1
 SELECT school, first_name, last_name FROM teachers ORDER BY school, last_name;
+
+-- Exercise 2
+SELECT * FROM teachers WHERE first_name ILIKE 's%' AND salary > 40000;
