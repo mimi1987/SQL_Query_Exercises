3fc65e7e6b52f3ae33e85c99757fcc5e71f2adbe
Michael M <m.migsch@gmail.com>
2020-04-22 21:15:30 +0200

    Update film.sql

--- film.sql
+++ film.sql
@@ -9,3 +9,18 @@ GROUP BY
 ORDER BY
     2 desc
 ;
+
+
+# What was the first rental date and the last?
+
+SELECT
+    f.title as 'Movie', max(left(r.rental_date, 10)) as 'Last Rental', min(left(r.rental_date, 10)) as 'First Rental'
+FROM
+    film f, inventory i, rental r
+WHERE
+    f.film_id = i.film_id and i.inventory_id = r.inventory_id
+GROUP BY
+    f.film_id
+ORDER BY
+    1
+;
